eec139c056ea2f113b748ae98b50e992ccf9d3ce
Jefferson gomes <jefferson.silva@medral.com.br>
2020-10-18 18:46:18 -0300

    PR: Efetuando Reserva com Post MySQL

---
+++ inc/reservations.js
@@ -0,0 +1,49 @@
+const { fileLoader } = require("ejs");
+var conn = require("./db");
+
+module.exports = {
+
+  render(req, res, error, success){
+
+    res.render('reservations', {
+      title: 'Reservation - Restaurante Saboroso!',
+      background: 'images/img_bg_2.jpg',
+      h1: 'Reserve uma Mesa!',
+      body: req.body, error, success
+    });
+
+  },
+
+  save(fields) {
+
+    return new Promise((resolve, reject ) => {
+
+      let date = fields.date.split('/');
+
+      fields.date = `${date[2]}-${date[1]}-${[0]}`;
+
+      conn.query(`
+          INSERT INTO tb_reservations (name, email, people, date, time)
+          VALUES(?, ?, ?, ?, ?)
+      `, [
+        fields.name,
+        fields.email,
+        fields.people,
+        fields.date,
+        fields.time
+
+      ], (err, results) => {
+
+          if(err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+
+      });
+
+    });
+
+  }
+
+};
